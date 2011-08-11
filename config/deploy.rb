723ff44c7c1de9445376add1c06fca7085005583
akhilesh <kataria.subs@gmail.com>
2011-08-11 18:20:04 +0530

    capify fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :user, 'rails'
 
 # Get repo configuration
 set :scm, "git"
-set :scm_username, 'akil_rails'
+set :scm_username, 'akil-rails'
 set :repository, "git@github.com:#{scm_username}/#{application}.git"
 set :branch, "master"
 set :deploy_via, :remote_cache
